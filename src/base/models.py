139332311b19ff14ecca1d29c6ed5d4c22479b21
BrainIsDead <pro100Istomin1988@gmail.com>
2023-05-08 16:52:08 +0300

    extended  LinkTypeChoices

--- src/base/models.py
+++ src/base/models.py
@@ -8,7 +8,13 @@ class LinkTypeChoices(models.TextChoices):
     """
     API_DOCUMENTATION = 'api_documentation', _('API Documentation')
     DUA = 'dua', _('DUA')
+    INTERPRETING_MASK = 'interpreting_mask', _('Interpreting mask use in context')
+    QUESTION_TEXT = 'question_text', _('Question text')
     SURVEY_DETAILS = 'survey_details', _('SURVEY_DETAILS')
+    SURVEY_DOCUMENTATION = 'survey_documentation', _('Survey documentation')
+    TECHNICAL_DESCRIPTION = 'technical_description', _('Technical description')
+    WAVE_10_REVISION = 'wave_10_revision', _('Wave 10 revision updates')
+    WAVE_11_REVISION = 'wave_11_revision', _('Wave 11 revision updates')
     OTHER = 'other', _('Other')
 
 
