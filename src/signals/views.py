5b4c18de16b5dd8f5ffeb232ae262020453cb878
BrainIsDead <pro100Istomin1988@gmail.com>
2023-05-31 17:00:33 +0300

    pagination

--- src/signals/views.py
+++ src/signals/views.py
@@ -8,6 +8,7 @@ class SignalsListView(ListView):
 
     model = Signal
     template_name = 'signals/signal_list.html'
+    paginate_by = 2
 
     def get_queryset(self):
         queryset = super().get_queryset()
@@ -18,4 +19,9 @@ class SignalsListView(ListView):
         context = super().get_context_data(**kwargs)
         context['signals'] = self.get_queryset()
         context['filter'] = SignalFilter(self.request.GET, queryset=self.get_queryset())
+        page_obj = context['page_obj']
+        if page_obj.has_previous():
+            context['previous_page_number'] = page_obj.previous_page_number()
+        if page_obj.has_next():
+            context['next_page_number'] = page_obj.next_page_number()
         return context
