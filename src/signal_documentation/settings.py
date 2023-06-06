de731d90875714a38be3b5863fdf649c29d227eb
BrainIsDead <pro100Istomin1988@gmail.com>
2023-06-06 16:09:52 +0300

    pagination refactoring

--- src/signal_documentation/settings.py
+++ src/signal_documentation/settings.py
@@ -82,6 +82,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 
--- src/signals/views.py
+++ src/signals/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.views.generic import ListView
 
 from signals.filters import SignalFilter
@@ -8,7 +9,7 @@ class SignalsListView(ListView):
 
     model = Signal
     template_name = 'signals/signal_list.html'
-    paginate_by = 2
+    paginate_by = 10
 
     def get_queryset(self):
         queryset = super().get_queryset()
@@ -17,11 +18,9 @@ class SignalsListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['signals'] = self.get_queryset()
         context['filter'] = SignalFilter(self.request.GET, queryset=self.get_queryset())
-        page_obj = context['page_obj']
-        if page_obj.has_previous():
-            context['previous_page_number'] = page_obj.previous_page_number()
-        if page_obj.has_next():
-            context['next_page_number'] = page_obj.next_page_number()
+        paginator = Paginator(self.get_queryset(), self.paginate_by)
+        page_number = self.request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        context['signals'] = page_obj
         return context
