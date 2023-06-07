70cd4cd3f48f2921df026e7085db6a602cf6b029
BrainIsDead <pro100Istomin1988@gmail.com>
2023-06-07 17:14:10 +0300

    signal detail

--- src/signals/urls.py
+++ src/signals/urls.py
@@ -4,5 +4,5 @@ from signals.views import SignalsDetailView, SignalsListView
 
 urlpatterns = [
     path('', SignalsListView.as_view(), name='signals'),
-    path('signals/<pk>/', SignalsDetailView.as_view(), name='signals'),
+    path('signals/<pk>/', SignalsDetailView.as_view(), name='signal'),
 ]
