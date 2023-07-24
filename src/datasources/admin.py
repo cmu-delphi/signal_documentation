cce97a4447692f5868b4627fe38dcd12ee7fd2ad
BrainIsDead <pro100Istomin1988@gmail.com>
2023-07-24 19:59:09 +0300

    Update data source admin and resource classes, remove redundant migrations and add a new admin interface for managing signal categories.

--- src/datasources/admin.py
+++ src/datasources/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
 
 from datasources.models import DataSource, SourceSubdivision
-from datasources.resources import DataSourceResource, SourceSubdivisionResource
+from datasources.resources import SourceSubdivisionResource
 
 
 @admin.register(SourceSubdivision)
@@ -22,4 +22,3 @@ class DataSourceAdmin(ImportExportModelAdmin):
     """
     list_display = ('name',)
     search_fields = ('name', 'source_subdivision__db_source', 'source_subdivision__name', 'description')
-    resource_classes = [DataSourceResource]
--- src/datasources/resources.py
+++ src/datasources/resources.py
@@ -7,67 +7,6 @@ from base.models import Link, LinkTypeChoices
 from datasources.models import DataSource, SourceSubdivision
 
 
-class DataSourceResource(resources.ModelResource):
-    """
-    A resource for importing and exporting data sources using the Django ORM.
-    """
-    name = Field(attribute='name', column_name='Name')
-    display_name = Field(attribute='display_name', column_name='Name')
-    description = Field(attribute='description', column_name='Description')
-    source_license = Field(attribute='source_license', column_name='License')
-    links = Field(
-        attribute='links',
-        column_name='Links',
-        widget=widgets.ManyToManyWidget(Link, field='url', separator='|'),
-    )
-    source_subdivisions = Field(
-        attribute='source_subdivisions',
-        column_name='Name',
-        widget=widgets.ForeignKeyWidget(DataSource, field='name'),
-    )
-
-    class Meta:
-        model = DataSource
-        fields = ('name', 'display_name', 'description', 'source_license', 'links')
-        import_id_fields = ['name']
-
-    def before_import_row(self, row, **kwargs):
-        """
-        Hook called before importing each row. Modifies 'Links' column to include
-        any additional links specified in 'DUA' or 'Link' columns.
-        """
-        self.process_links(row)
-        self.process_subdivisions(row)
-
-    def process_subdivisions(self, row):
-        if row['Source Subdivision']:
-            data_source = DataSource.objects.get(name=row['Name'])
-            source_subdivision, created = SourceSubdivision.objects.get_or_create(
-                name=row['Source Subdivision'],
-                defaults={
-                    'display_name': row['Source Subdivision'],
-                    'description': row['Description'],
-                    'db_source': row['DB Source'],
-                    'data_source': data_source
-                }
-            )
-
-    def process_links(self, row):
-        row['Links'] = ''
-        if row['DUA']:
-            link, created = Link.objects.get_or_create(url=row['DUA'], defaults={'link_type': LinkTypeChoices.DUA})
-            row['Links'] += row['Links'] + f'|{link.url}'
-
-        if row['Link']:
-            pattern = r'\[(.*?)\]\((.*?)\)'
-            pattern_match = re.search(pattern, row['Link'])
-            link_type_mapping = {choice.label: choice.value for choice in LinkTypeChoices}
-            link_type = link_type_mapping[pattern_match.group(1)]
-            link_url = pattern_match.group(2)
-            link, created = Link.objects.get_or_create(url=link_url, link_type=link_type)
-            row['Links'] += row['Links'] + f'|{link.url}'
-
-
 class SourceSubdivisionResource(resources.ModelResource):
     name = Field(attribute='name', column_name='Source Subdivision')
     display_name = Field(attribute='display_name', column_name='Source Subdivision')
@@ -86,7 +25,7 @@ class SourceSubdivisionResource(resources.ModelResource):
 
     class Meta:
         model = SourceSubdivision
-        fields = ('name', 'display_name', 'description', 'links', 'data_source')
+        fields = ('name', 'display_name', 'description', 'data_source', 'reference_signal', 'links')
         import_id_fields = ['name']
         skip_unchanged = True
 
@@ -95,6 +34,10 @@ class SourceSubdivisionResource(resources.ModelResource):
         Hook called before importing each row. Modifies 'Links' column to include
         any additional links specified in 'DUA' or 'Link' columns.
         """
+        self.process_links(row)
+        self.process_datasource(row)
+
+    def process_links(self, row):
         row['Links'] = ''
         if row['DUA']:
             link, created = Link.objects.get_or_create(url=row['DUA'], link_type=LinkTypeChoices.DUA)
@@ -107,3 +50,16 @@ class SourceSubdivisionResource(resources.ModelResource):
             link_url = pattern_match.group(2)
             link, created = Link.objects.get_or_create(url=link_url, link_type=link_type)
             row['Links'] += row['Links'] + f'|{link.url}'
+
+    def process_datasource(self, row):
+        if row['Name']:
+            data_source, created = DataSource.objects.get_or_create(
+                name=row['Name'],
+                defaults={
+                    'display_name': row['Name'],
+                    'description': row['Description'],
+                    'source_license': row['License'],
+                }
+            )
+            links = Link.objects.filter(url__in=row['Links'].split('|')).values_list('id', flat=True)
+            data_source.links.add(*links)
