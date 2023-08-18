aed54c6f7dfd22746ebb73ba6019ce867b6bc733
BrainIsDead <pro100Istomin1988@gmail.com>
2023-08-18 17:00:36 +0300

    minor

--- src/signal_documentation/settings.py
+++ src/signal_documentation/settings.py
@@ -183,6 +183,7 @@ MEDIA_ROOT: str = os.path.join(BASE_DIR, 'media')
 
 MAIN_PAGE = os.environ.get('MAIN_PAGE', '')
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
