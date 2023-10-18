98a232f9b5073e7e45540125f9f10f33c6789f5b
BrainIsDead <pro100Istomin1988@gmail.com>
2023-10-18 17:50:04 +0300

    The changes made in this commit include: - Modified `test_views.py` to change the loop variable from `i` to `_` in the `setUp` method. - Modified `urls.py` to specify that the `pk` parameter in the URL pattern for the `SignalsDetailView` should be an integer. - Modified `signal_list.html` to add a conditional statement that checks if `signal.base` exists before displaying the link to the detail view.

--- src/signals/urls.py
+++ src/signals/urls.py
@@ -5,5 +5,5 @@ from signals.views import SignalsDetailView, SignalsListView
 
 urlpatterns: list[URLPattern] = [
     path('', SignalsListView.as_view(), name='signals'),
-    path('signals/<pk>/', SignalsDetailView.as_view(), name='signal'),
+    path('signals/<int:pk>/', SignalsDetailView.as_view(), name='signal'),
 ]
