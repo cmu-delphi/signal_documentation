c8f77d9b24a09480d127dfdece2a750c54171c64
Dmytro Trotsko <dmytrotsko@gmail.com>
2023-11-14 01:15:56 +0200

    Fix preserve search params in URL after page change. Add preserve selected filters in filters list

--- src/signals/views.py
+++ src/signals/views.py
@@ -20,13 +20,29 @@ class SignalsListView(ListView):
         f = SignalFilter(self.request.GET, queryset=queryset)
         return f.qs
 
+    def get_url_params(self):
+        url_params_dict = dict()
+        url_params_str = ""
+        for param_name in self.request.GET:
+            if param_name == "page":
+                continue
+            param_value = self.request.GET._getlist(param_name)
+            if param_name != "search":
+                url_params_dict[param_name] = [int(el) for el in param_value if el != ""]
+                for v in param_value:
+                    url_params_str = f"{url_params_str}&{param_name}={v}"
+            else:
+                url_params_dict[param_name] = self.request.GET[param_name]
+
+        return url_params_dict, url_params_str
+
     def get_context_data(self, **kwargs) -> Dict[str, Any]:
         context: Dict[str, Any] = super().get_context_data(**kwargs)
         context['filter'] = SignalFilter(self.request.GET, queryset=self.get_queryset())
         paginator = Paginator(self.get_queryset(), self.paginate_by)
         page_number: str | None = self.request.GET.get('page')
         page_obj: Page = paginator.get_page(page_number)
-        context["url_params"] = "&".join([f"{p}={self.request.GET[p]}" for p in context["filter"]._meta.fields if p in self.request.GET])
+        context["url_params_dict"], context["url_params_str"] = self.get_url_params()
         context['signals'] = page_obj
         return context
 
