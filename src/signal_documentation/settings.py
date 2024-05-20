94abca54b99f9592308607886ed5a631583c872b
Dmytro Trotsko <dmytrotsko@gmail.com>
2024-05-20 17:12:29 +0300

    Update settings.py
    Used os.env to get variables

--- src/signal_documentation/settings.py
+++ src/signal_documentation/settings.py
@@ -23,8 +23,8 @@ import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 from sentry_sdk.integrations.redis import RedisIntegration
 
-EPIVIS_URL = "https://deploy-preview-36--cmu-delphi-epivis.netlify.app/"
-DATA_EXPORT_URL = "https://api.covidcast.cmu.edu/epidata/covidcast/csv"
+EPIVIS_URL = os.environ.get("EPIVIS_URL", "https://deploy-preview-36--cmu-delphi-epivis.netlify.app/")
+DATA_EXPORT_URL = os.environ.get("DATA_EXPORT_URL", "https://api.covidcast.cmu.edu/epidata/covidcast/csv")
 
 SENTRY_DSN = os.environ.get('SENTRY_DSN')
 if SENTRY_DSN:
